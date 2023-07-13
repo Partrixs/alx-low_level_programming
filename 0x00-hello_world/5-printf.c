5e85364c2426d6c9469dcbbcc45933a032a59714
Partrixs <patricknjoroge092@gmail.com>
2023-07-13 06:36:09 -0700

    Hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This displays the main function
+ *
+ * Return: o when successful
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
