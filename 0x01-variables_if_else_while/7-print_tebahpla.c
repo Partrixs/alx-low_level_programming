f92631262bfdcfea20d68033a0dc88d028fc6739
Partrixs <patricknjoroge092@gmail.com>
2023-07-14 10:59:38 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char m;
+
+	m = 'z';
+	while
+		(m >= 'a') {
+			putchar(m);
+			m--;
+		}
+	putchar('\n');
+	return (0);
+}
