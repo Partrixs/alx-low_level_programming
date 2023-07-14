cecd4b26a7a0c0474fe628b4948211d42dc9cda4
Partrixs <patricknjoroge092@gmail.com>
2023-07-14 11:45:11 -0700

    Combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int j;
+
+	for (j = 0; j < 100; j++)
+	{
+		putchar(j + '0');
+		if (j < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
