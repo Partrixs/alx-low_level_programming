7a74d5ae81397572855c8b534c012e36df9d9cc5
Partrixs <patricknjoroge092@gmail.com>
2023-07-14 09:58:24 -0700

    Alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Alphabet lowcase printing
+ *Return: 0 (Succes)
+ */
+
+int main(void)
+{
+	char k;
+
+	k = 'a';
+	while
+		(k <= 'z') {
+			putchar(k);
+			k++;
+		}
+	putchar('\n');
+	return (0);
+}
