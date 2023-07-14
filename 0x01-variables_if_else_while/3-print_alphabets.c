ef3ebc8b8bf6c333f5e771660b400cce432c3761
Partrixs <patricknjoroge092@gmail.com>
2023-07-14 10:12:09 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char m;
+
+	char n;
+
+	m = 'j';
+	n = 'J';
+	while
+		(m <= 'z') {
+			putchar(m);
+			m++;
+		}
+	while
+		(n <= 'Z') {
+			putchar(n);
+			n++;
+		}
+	putchar('\n');
+	return (0);
+}
