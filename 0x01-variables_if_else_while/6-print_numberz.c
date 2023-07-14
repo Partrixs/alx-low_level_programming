7a9fc9559af3195cd0d4e5c7e78509598c9e6606
Partrixs <patricknjoroge092@gmail.com>
2023-07-14 10:53:03 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int z;
+
+	z = 0;
+	while
+		(z < 10) {
+			putchar(z + '0');
+			z++;
+		}
+	putchar('\n');
+	return (0);
+}
