17ea7fea940fd0fd32c145f863d2247a1b9a2d69
Partrixs <patricknjoroge092@gmail.com>
2023-07-14 10:44:56 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int z;
+
+	for (z = 0; z < 10; z++)
+		printf("%d", z);
+	putchar('\n');
+	return (0);
+}
