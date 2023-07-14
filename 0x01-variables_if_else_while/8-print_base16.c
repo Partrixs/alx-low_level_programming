9bc42fccaec193a13b56370da5168238e1f388cf
Partrixs <patricknjoroge092@gmail.com>
2023-07-14 11:23:50 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char e;
+
+	int j;
+
+	e = 'a';
+	j = 0;
+	while
+		(j < 10) {
+			putchar(j + '0');
+			j++;
+		}
+	while
+		(e <= 'f') {
+			putchar(e);
+			e++;
+		}
+
+	putchar('\n');
+	return (0);
+}
