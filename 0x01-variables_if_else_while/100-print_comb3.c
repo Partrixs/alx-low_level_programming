30e6bee166bd39bffdf19ea158fc24b6285e6a95
Partrixs <patricknjoroge092@gmail.com>
2023-07-14 17:47:47 -0700

    Invention and material

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /**
  * main - Starting point
  *
@@ -7,38 +10,26 @@
  * Return: 0 (Success)
  */
 int main(void)
+
 {
-	int num1, num2, num3;
 
-	num1 = '0';
-	num2 = '0';
-	num3 = '0';
+	int a, b;
 
-	while (num1 <= '9')
+	for (a = '0'; a < '9'; a++)
 	{
-		while (num2 <= '9')
-		{
-			num3 = '0';
-			while (num3 <= '9')
-			{
-				if (num1 < num2 && num2 < num3)
-				{
-					putchar(num1);
-					putchar(num2);
-					putchar(num3);
 
-					if (num1 != '7')
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				num3++;
-			}
-			num2++;
-		}
-		num1++;
-		num2 = '0';
+	for (b = a + 1; b <= '9'; b++)
+	{
+	if (b != a)
+	{
+	putchar(a);
+	putchar(b);
+	if (a == '8' && b == '9')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
 	}
 	putchar('\n');
 	return (0);
