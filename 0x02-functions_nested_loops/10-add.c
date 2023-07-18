009eb96c141fedd3a7a9185e21ad37c0c84b8a98
Partrixs <patricknjoroge092@gmail.com>
2023-07-18 14:30:10 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ * @i1: The first digit to be computed
+ * @i2: The second digit to ber computed
+ * Return: Integer value
+ */
+
+int add(int i1, int i2)
+
+{
+
+	return (i1 + i2);
+}
