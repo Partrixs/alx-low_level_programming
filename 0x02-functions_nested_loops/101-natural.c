9d717d4f28a36c6a5d1258072ce09b45276575cc
Partrixs <patricknjoroge092@gmail.com>
2023-07-18 16:33:39 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints sum of multiples of 3 or 5 up to 1024
+ * Return: (Success)
+ */
+
+int main(void)
+
+{
+	int b, j = 0;
+
+	while (b < 1024)
+	{
+	if ((j % 3 == 0) || (j % 5 == 0))
+	{
+	j += b;
+	}
+	b++;
+	}
+	printf("%d\n", j);
+	return (0);
+}
