7d58be0b796198915a212cbd5cde46790570647c
Partrixs <patricknjoroge092@gmail.com>
2023-07-18 13:51:40 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table - Prints 9 times tables starting 0
+ * Return: output none
+ */
+
+void times_table(void)
+
+{
+
+	int a, b, c, y, z;
+
+	for (a = 0; a <= 9; a++)
+	{
+	for (b = 0; b <= 9; b++)
+	{
+	c = a + b;
+	if (c > 9)
+	{
+	y = z % 10;
+	z = (c - y) / 10;
+	_putchar(44);
+	_putchar(32);
+	_putchar(z + '0');
+	_putchar(y + '0');
+	}
+	else
+	{
+	if (b != 0)
+	{
+	_putchar(44);
+	_putchar(32);
+	_putchar(32);
+	}
+	_putchar(c + '0');
+	}
+	}
+	_putchar('\n');
+	}
+}
