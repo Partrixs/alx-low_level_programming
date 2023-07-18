16c90ccf4acd39009874b9f204ff242cdbd5d590
Partrixs <patricknjoroge092@gmail.com>
2023-07-18 06:50:36 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Make alphabet x10 times
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+
+{
+
+	char b;
+	int i = 0;
+
+	while (i <= 9)
+	{
+	for (b = 'a'; b <= 'z'; b++)
+	{
+	_putchar(b);
+	}
+	_putchar('\n');
+	i++;
+	}
+}
