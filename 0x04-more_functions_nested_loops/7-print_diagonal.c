fed2e851d53d1c200b3bc6f3eacfeb359dbdc537
Partrixs <patricknjoroge092@gmail.com>
2023-07-20 14:07:58 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,18 +8,20 @@
 
 void print_diagonal(int n)
 {
+	int a, b;
+
 	if (n <= 0)
-		_putchar('\n');
+	{
+	_putchar('\n');
+	}
 	else
 	{
-		int a, b;
-
-		for (a = 0; a < n; a++)
-		{
-			for (b = 0; b <= a; b++)
-				_putchar(' ');
-			_putchar('\\');
-			_putchar('\n');
-		}
+	for (a = 0; a < n; a++)
+	{
+	for (b = 0; b < a; b++)
+	_putchar(32);
+	_putchar('92');
+	_putchar('\n');
+	}
 	}
 }
