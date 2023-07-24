41df8e89e36944ea67e72308bc6e114379a55ffb
Partrixs <patricknjoroge092@gmail.com>
2023-07-24 07:58:30 -0700

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of int  parameter  to 98
+ * @n: the input value
+ * rEturn: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <unistd>
+#include <unistd.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -13,7 +13,7 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-void print_array(int *a int n);
+void print_array(int *a, int n);
 char *_strcpy(char *dust, char *src);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
