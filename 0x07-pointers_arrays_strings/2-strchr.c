acaf15aeb54165b1d6dbadc843cb78fdd8bf03a2
Partrixs <patricknjoroge092@gmail.com>
2023-07-31 03:38:22 -0700

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: 0 (Success)
+ */
+
+char *_strchr(char *s, char c)
+{
+	int f;
+
+	for (f = 0; s[f] >= '\0'; f++)
+	{
+	if (s[f] == c)
+		return (s + f);
+	}
+
+	return (NULL);
+}
