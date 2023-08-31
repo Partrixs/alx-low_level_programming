a2862badec2d52d1b031874d87bfe7cd032c392d
Partrixs <patricknjoroge092@gmail.com>
2023-08-31 19:50:51 +0300

    0. 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int p;
+	unsigned int dig_dec = 0;
+
+	if (!b)
+		return (0);
+
+	for (p = 0; b[p]; p++)
+	{
+		if (b[p] < '0' || b[p] > '1')
+			return (0);
+		dig_dec = 2 * dig_dec + (b[p] - '0');
+	}
+
+	return (dig_dec);
+}
